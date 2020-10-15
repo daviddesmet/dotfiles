3f28d00a78f694742771cff388ff52921d45c4eb
David De Smet <2607383+daviddesmet@users.noreply.github.com>
2020-10-15 11:29:08 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -38,7 +38,7 @@ then
     fonts-powerline \
     zsh
 
-    zsh
+    # zsh
     # git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
     # setopt EXTENDED_GLOB
     # for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
